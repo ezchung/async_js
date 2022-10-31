438ed156c6bf3b5d60c79082498333b6a8f2b87a
Ezra Chung <ezra.chung97@gmail.com>
2022-10-31 16:17:54 -0700

    Initial take

--- number_facts.js
+++ number_facts.js
@@ -1,3 +1,17 @@
 "use strict";
 
-const NUMBERS_API = "http://numbersapi.com/"
+const NUMBERS_API_URL = "http://numbersapi.com/";
+const $factsSection = $("#facts_list");
+
+/** Takes inputted numbers, queries the NUMBERS API and appends the facts from
+ * NUMBERS API into the DOM.
+ */
+async function getNumbersFacts(...numbers) {
+  let params = numbers.join(",");
+
+  const facts = await axios({ url: NUMBERS_API_URL + params });
+
+  for (let fact in facts.data) {
+    $factsSection.append($(`<li>${facts.data[fact]}</li>`));
+  }
+}
