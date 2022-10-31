2071a011046144cd76574298769bcec2677577b8
Ezra Chung <ezra.chung97@gmail.com>
2022-10-31 16:54:33 -0700

    Created getNumberFact function

--- number_facts.js
+++ number_facts.js
@@ -7,11 +7,30 @@ const $factsSection = $("#facts_list");
  * NUMBERS API into the DOM.
  */
 async function getNumbersFacts(...numbers) {
-  let params = numbers.join(",");
+  $factsSection.empty();
+    let params = numbers.join(",");
 
-  const facts = await axios({ url: NUMBERS_API_URL + params });
-
+  const facts = await axios({ url: NUMBERS_API_URL + params + '?json' });
+//   console.log(facts)
   for (let fact in facts.data) {
+    // console.log(text)
     $factsSection.append($(`<li>${facts.data[fact]}</li>`));
   }
 }
+
+/** Takes inputted number, queries the NUMBERS API and appends 4 facts from
+ * NUMBERS API into the DOM. */
+async function getNumberFact(number) {
+  $factsSection.empty();
+
+  const fact1 = axios({ url: NUMBERS_API_URL + number + '?json' });
+  const fact2 = axios({ url: NUMBERS_API_URL + number + '?json' });
+  const fact3 = axios({ url: NUMBERS_API_URL + number + '?json' });
+  const fact4 = axios({ url: NUMBERS_API_URL + number + '?json' });
+  let resp = await Promise.allSettled([fact1, fact2, fact3, fact4]);
+
+  for(let fact of resp){
+    $factsSection.append($(`<li>${fact.value.data.text}</li>`));
+  }
+}
+
